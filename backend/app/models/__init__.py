345010a461b895bb8e39ea6734671bb6613d720b
cgrubio91 <cgrubio91@gmail.com>
2026-01-06 09:21:50 -0500

    reinicio de proyecto

--- backend/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User, UserRole
-from app.models.project import Project, ProjectMember, ProjectMemberRole
-from app.models.layer import Layer, LayerType, LayerFormat
-from app.models.measurement import Measurement, MeasurementType, MeasurementUnit
--- backend/app/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import auth
-from . import user
-from . import project
-from . import layer
-from . import measurement
